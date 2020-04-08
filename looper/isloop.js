e3819d4a5ca915387f73c199ea3bb04525a5e56a
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2020-04-08 16:18:24 -0400

    diagrammed possible solution

--- looper/isloop.js
+++ looper/isloop.js
@@ -7,6 +7,16 @@ const isLoop = (linkedlist) => {
 
 
 /*
+1. have storage DT arr=[]
+2. traverse the LL and push in their values,
+3. conditionals: if value exists, return true
+    if we get to the tail, return false
+4. access each node with getNthNode(n) method
+[1,2,3,4,5,6,          7,8,9,10]
+            3, 4, 5, 6
+
+
+
 EXTRA CREDIT:
 
 Write a function findLoop() that consumes a linkedlist with a loop
