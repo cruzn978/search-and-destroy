3637d4d278b625376e491ce79a86711d3ce1d6b3
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2020-04-08 17:51:36 -0400

    min jumps init

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const minJumps = arr => {
 
+
+const minJumps = (arr) => {
+  let jumps = 0;
+  let pointer = 0;
+  let currVal = arr[pointer]
+  if (arr[pointer] >= arr.length - 1) {
+    jumps++;
+    return jumps;
+  } else {
+    let newArr = arr.slice(pointer + 1, currVal + 1)
+    let largestNum = Math.max(...newArr) // 8
+    let index = arr.indexOf(largestNum)
+    jumps++
+    return minJumps(arr.splice(pointer,index)) + jumps
+  }
 };
 
-module.exports = minJumps
+/* [8,4]
+[2,8,4,3,2,9,6,8]
+evaluate currNum
+  const jumps = 0
+  arr.splice(currNumIndex)
+  if currNum >= array.length {jumps++ return jumps}
+
+evaluate currNum
+find the largest number in its range
+  {jump to that number, jump++, currNum = newNum}
+
+  REPEAT
+*/
+
+module.exports = minJumps;
